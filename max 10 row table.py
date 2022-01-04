3f27a56166e3963e4d64019f17535b4388ec0a15
Mi Monir <40669686+mimonirbd@users.noreply.github.com>
2022-01-04 18:04:40 +0600

    Update max 10 row table.py

--- max 10 row table.py
+++ max 10 row table.py
@@ -1,13 +1,13 @@
-        def top_10(dictionary, max_entries=10):
-            if isinstance(dictionary, str):
-                return '<td>' + dictionary + '</td>'
-            s = ['<!-- wp:table --><figure class="wp-block-table"><table><tbody>']
-            for n, [key, value] in enumerate(dictionary.items()):
-                if n == max_entries:
-                    break
-                s.append('<tr><td>%s</td>' % key)
-                s.append('<td>%s</td>' % value)
-                s.append('</tr>')
-            s.append('</tbody></table></figure><!-- /wp:table -->')
-            return ''.join(s)
-        # print(top_10(dicts))
+def top_10(dictionary, max_entries = 10):
+    if isinstance(dictionary, str):
+        return '<td>'+dictionary+'</td>'
+    s = ['<!-- wp:table --><figure class="wp-block-table"><table><tbody>']
+    for n, [key, value] in enumerate(dictionary.items()):
+        if n == max_entries:
+            break
+        s.append('<tr><td>%s</td>' % key)
+        # s.append('<tr><td>'+key+'</td>')
+        s.append('<td>%s</td>' % value)
+        s.append('</tr>')
+    s.append('</tbody></table></figure><!-- /wp:table -->')
+    return ''.join(s)
